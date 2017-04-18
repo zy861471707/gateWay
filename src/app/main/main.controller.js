955f84a4cd22e245a3b66b83d760e094f55d925d
zhangyang <861471707@qq.com>
2017-04-18 23:35:10 +0800

    修改注册登录设计

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,12 +6,11 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  // MainController.$inject = ['$timeout','$scope','$state'];
   function MainController($timeout,$state,$log) {
     var vm = this;
+    
   	vm.register = function(){
       console.log(11111)
-  		$state.go('login');
   	};
   };
 })();
